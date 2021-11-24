5c34d85d4498552003699c526de2e3a1414b4644
johnoh <johnoh@usc.edu>
2021-11-24 15:49:17 -0500

    Updates

---
+++ precache-manifest.724b8f97644f48d45588276b7db646c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92a9be2ac423dc203a694bbeb8eb358a",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "42fc605d464a95f5980d",
+    "url": "/React-Portfolio/static/css/main.aef3b975.chunk.css"
+  },
+  {
+    "revision": "23b235dee34c2e54622a",
+    "url": "/React-Portfolio/static/js/2.bba5455a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/React-Portfolio/static/js/2.bba5455a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42fc605d464a95f5980d",
+    "url": "/React-Portfolio/static/js/main.953ddd7f.chunk.js"
+  },
+  {
+    "revision": "1f40be0c0d226896706d",
+    "url": "/React-Portfolio/static/js/runtime-main.481932d9.js"
+  }
+]);
